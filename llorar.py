e53ddb5e3ba16146f4c450c925076b481aa37539
estarducs <fernando.concha@uc.cl>
2023-04-11 21:55:18 -0400

    Se agreaga código

---
+++ llorar.py
@@ -0,0 +1,4 @@
+def llorar(nombre_llorante,horas_lloradas):
+    print(nombre_llorante,"esta extremadamente triste por razones personales y académicas, por lo que llorara",horas_lloradas,"horas.")
+
+print(llorar(input("Ingrese el nombre del llorante: " ),input("Ingrese las horas por llorar: ")))
